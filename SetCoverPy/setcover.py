b380033ad593d43b35e0ea4e758c4782535102a5
AnSq <ansq00@gmail.com>
2025-03-02 13:53:28 -0800

    replace print statements with logging

--- SetCoverPy/setcover.py
+++ SetCoverPy/setcover.py
@@ -13,16 +13,16 @@ __version__ = "0.9.0"
 
 __all__ = ['SetCover']
 
-""" 
+"""
 setcover.py
 
-    This piece of software is developed and maintained by Guangtun Ben Zhu, 
-    It is designed to find an (near-)optimal solution to the set cover problem (SCP) as 
-    fast as possible. It employs an iterative heuristic approximation method, combining 
+    This piece of software is developed and maintained by Guangtun Ben Zhu,
+    It is designed to find an (near-)optimal solution to the set cover problem (SCP) as
+    fast as possible. It employs an iterative heuristic approximation method, combining
     the greedy and Lagrangian relaxation algorithms.
 
-    For the standard tests (4,5,6, A-H instances from Beasley's OR Library), the code yields 
-    solutions that are 96%-100% optimal (see scp_BeasleyOR_test_results.txt). 
+    For the standard tests (4,5,6, A-H instances from Beasley's OR Library), the code yields
+    solutions that are 96%-100% optimal (see scp_BeasleyOR_test_results.txt).
 
     As all the codes, this code can always be improved and any feedback will be greatly appreciated.
 
@@ -41,19 +41,19 @@ setcover.py
 
     Copyright (c) 2015-2016 Guangtun Ben Zhu
 
-    Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
-    and associated documentation files (the "Software"), to deal in the Software without 
-    restriction, including without limitation the rights to use, copy, modify, merge, publish, 
-    distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the 
+    Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+    and associated documentation files (the "Software"), to deal in the Software without
+    restriction, including without limitation the rights to use, copy, modify, merge, publish,
+    distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
     Software is furnished to do so, subject to the following conditions:
 
-    The above copyright notice and this permission notice shall be included in all copies or 
+    The above copyright notice and this permission notice shall be included in all copies or
     substantial portions of the Software.
 
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
-    BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
-    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
-    DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+    BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+    DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 """
@@ -65,6 +65,9 @@ import numpy as np
 # Using the dot product in the sparse module can speed up set intersection operation drastically
 from scipy import sparse
 from time import time
+import logging
+
+logger = logging.getLogger(__name__)
 
 # Some magic numbes
 _stepsize = 0.1
@@ -76,47 +79,47 @@ class SetCover:
     Set Cover Problem - Find a set of columns that cover all the rows with minimal cost.
 
     Algorithm:
-        -- greedy: 
+        -- greedy:
         -- Lagrangian relaxation:
-    Input: 
-        -- a_matrix[mrows, ncols], the covering binary matrix, 
+    Input:
+        -- a_matrix[mrows, ncols], the covering binary matrix,
            a_matrix[irow, jcol] = True if jcol covers irow
-        -- cost[ncols], the cost of columns. 
+        -- cost[ncols], the cost of columns.
            I recommend using normalized cost: cost/median(cost)
 
     (Use A.4 instance from Beasley's OR library,
      http://people.brunel.ac.uk/~mastjjb/jeb/orlib/scpinfo.html, as an example)
-    Instantiation: 
+    Instantiation:
         >> a_matrix = np.load('./BeasleyOR/scpa4_matrix.npy')
         >> cost = np.load('./BeasleyOR/scpa4_cost.npy')
         >> g = setcover.SetCover(a_matrix, cost)
-    Run the solver: 
+    Run the solver:
         >> solution, time_used = g.SolveSCP()
 
     Output:
-        -- g.s, the (near-optimal) minimal set of columns, a binary 1D array, 
+        -- g.s, the (near-optimal) minimal set of columns, a binary 1D array,
            g.s[jcol] = True if jcol is in the solution
         -- g.total_cost, the total cost of the (near-optimal) minimal set of columns
 
     Comments:
-        -- 
+        --
 
     References:
         -- Guangtun Ben Zhu, 2016
-           A New View of Classification in Astronomy with the Archetype Technique: 
+           A New View of Classification in Astronomy with the Archetype Technique:
            An Astronomical Case of the NP-complete Set Cover Problem
            AJ/PASP, (to be submitted)
-        -- Caprara, A., Fischetti, M. and Toth, P., 1999 
-           A heuristic method for the set covering problem 
+        -- Caprara, A., Fischetti, M. and Toth, P., 1999
+           A heuristic method for the set covering problem
            Operations research, 47(5), pp.730-743.
-        -- Fisher, M.L., 2004 
-           The Lagrangian relaxation method for solving integer programming problems 
+        -- Fisher, M.L., 2004
+           The Lagrangian relaxation method for solving integer programming problems
            Management science, 50(12_supplement), pp.1861-1871.
-        -- Balas, E. and Carrera, M.C., 1996 
-           A dynamic subgradient-based branch-and-bound procedure for set covering 
+        -- Balas, E. and Carrera, M.C., 1996
+           A dynamic subgradient-based branch-and-bound procedure for set covering
            Operations Research, 44(6), pp.875-890.
-        -- Beasley, J.E., 1990  
-           OR-Library: distributing test problems by electronic mail  
+        -- Beasley, J.E., 1990
+           OR-Library: distributing test problems by electronic mail
            Journal of the operational research society, pp.1069-1072.
         -- And many more, please see references in Zhu (2016).
 
@@ -136,12 +139,12 @@ class SetCover:
             amatrix
             cost
         Keywords:
-            maxiters - the maximum number of iterations for the re-initialization, 
+            maxiters - the maximum number of iterations for the re-initialization,
                        20 by default. This is the parameter you may want to increase
                        to get a better solution (at the expense of time)
             subg_nsteps - how many steps for each subgradient iteration, 15 by default
                           Note each step includes _subg_nadaptive (hard-coded at 20) mini steps
-            subg_maxiters - the maximum number of iterations for the subgradient phase, 
+            subg_maxiters - the maximum number of iterations for the subgradient phase,
                             100 by default
 
         """
@@ -231,7 +234,7 @@ class SetCover:
         Initial guess of the Lagrangian multiplier with random numbers
         """
         # Random is better to give different multipliers in the subgradient phase
-        return np.random.rand(self.mrows)*1. 
+        return np.random.rand(self.mrows)*1.
 
     def _u_naught(self):
         """
@@ -246,9 +249,9 @@ class SetCover:
         """
         Fix the unique columns that have to be in the minimal set
         """
-        # subgradient; for two boolean arrays, multiplication seems to be the best way 
+        # subgradient; for two boolean arrays, multiplication seems to be the best way
         # (equivalent to logical_and)
-        n_covered_col = self.a_csr.dot(np.ones(self.ncols)) 
+        n_covered_col = self.a_csr.dot(np.ones(self.ncols))
         ifix = np.zeros(self.ncols, dtype=bool)
         if (np.count_nonzero(n_covered_col) != self.mrows):
            raise ValueError("There are uncovered rows! Please check your input!")
@@ -264,7 +267,7 @@ class SetCover:
         Heuristic greedy method to select a set of columns to cover all the rows
         start from the initial set
         run the following first if you want to reset the initial selection with fixed columns
-            - self.reset_s() or 
+            - self.reset_s() or
             - self.s = np.logical_or(self.s, self.f)
         """
 
@@ -274,11 +277,11 @@ class SetCover:
 
         utmp = np.copy(u)
         iuncovered = ~np.any(self.a[:,self.s], axis=1)
-        
+
         score = np.zeros(self.ncols)
         while (np.count_nonzero(iuncovered) > 0) and (niters <= niters_max):
             # It's 5 times faster without indexing, the advantage is made possible by csc_matrix.dot
-            mu = (self.a_csc.dot((iuncovered).astype(int))).astype(float) 
+            mu = (self.a_csc.dot((iuncovered).astype(int))).astype(float)
             mu[mu<=_smallnumber] = _smallnumber
 
             utmp[~iuncovered] = 0
@@ -294,7 +297,7 @@ class SetCover:
             self.s[inewcolumn] = True
             iuncovered = ~np.logical_or(~iuncovered, self.a[:,inewcolumn])
             niters = niters+1
-        if (niters == niters_max): 
+        if (niters == niters_max):
            warnings.warn("Iteration in Greedy reaches maximum = {0}".format(niters_max))
         return self.total_cost
 
@@ -312,9 +315,9 @@ class SetCover:
 
     def subgradient(self):
         """
-        Subgradient step for the core problem N\S. 
+        Subgradient step for the core problem N\S.
         """
-        
+
         UB_full = self.total_cost
         ufull = np.copy(self.u)
 
@@ -329,7 +332,7 @@ class SetCover:
         cost = self.c[~self.f]
 
         # save nsteps calculations (Lagrangian multipliers and lower bounds)
-        u_sequence = np.zeros((mrows, self._subg_nsteps)) 
+        u_sequence = np.zeros((mrows, self._subg_nsteps))
         Lu_sequence = np.zeros(self._subg_nsteps)
         # update u
         x = np.zeros(ncols, dtype=bool)
@@ -347,28 +350,28 @@ class SetCover:
             u_sequence[:,0] = u_this
             cost_u = cost - a_csc.dot(u_sequence[:,0]) # Lagrangian cost
             # next lower bound of the Lagrangian subproblem
-            Lu_sequence[0] = np.einsum('i->', cost_u[cost_u<0])+np.einsum('i->', u_sequence[:,0]) 
+            Lu_sequence[0] = np.einsum('i->', cost_u[cost_u<0])+np.einsum('i->', u_sequence[:,0])
 
             for i in np.arange(self._subg_nsteps-1):
                 # current solution to the Lagrangian subproblem
                 x[0:] = False
                 x[cost_u<0] = True
 
-                # subgradient; for two boolean arrays, multiplication seems to be the best way 
+                # subgradient; for two boolean arrays, multiplication seems to be the best way
                 # (equivalent to logical_and)
-                s_u = 1. - a_csr.dot(x.astype(int)) 
+                s_u = 1. - a_csr.dot(x.astype(int))
                 s_u_norm = np.einsum('i,i',s_u,s_u) # subgradient's norm squared
 
                 # Update
                 # next Lagrangian multiplier
-                u_temp = u_sequence[:,i]+self._stepsize*(UB - Lu_sequence[i])/s_u_norm*s_u 
+                u_temp = u_sequence[:,i]+self._stepsize*(UB - Lu_sequence[i])/s_u_norm*s_u
                 u_temp[u_temp<0] = 0
 
                 u_sequence[:,i+1] = u_temp
                 cost_u = cost - a_csc.dot(u_sequence[:,i+1]) # Lagrangian cost
                 # next lower bound of the Lagrangian subproblem
-                Lu_sequence[i+1] = np.einsum('i->', cost_u[cost_u<0])+np.einsum('i->', u_sequence[:,i+1]) 
-            
+                Lu_sequence[i+1] = np.einsum('i->', cost_u[cost_u<0])+np.einsum('i->', u_sequence[:,i+1])
+
                 #print(UB_full, UB, Lu_sequence[i+1])
                 # Check the last nadaptive steps and see if the step size needs to be adapted
                 if (np.mod(i+1,self._subg_nadaptive)==0):
@@ -399,7 +402,7 @@ class SetCover:
             Lu_max0 = Lu_max # Just a copy. Not the reference (It's a number object)
             # save current u_this???
 
-            if (niters == niters_max): 
+            if (niters == niters_max):
                 warnings.warn("Iteration in subgradient reaches maximum = {0}".format(niters))
 
         # update multipliers
@@ -407,7 +410,7 @@ class SetCover:
 
         # return the last nsteps multipliers
         # save nsteps calculations (Lagrangian multipliers and lower bounds)
-        u_sequence_full = np.zeros((self.mrows, self._subg_nsteps)) 
+        u_sequence_full = np.zeros((self.mrows, self._subg_nsteps))
         Lu_sequence_full = np.zeros(self._subg_nsteps)
         u_sequence_full[self.f_covered,:] = self.u[self.f_covered][:, np.newaxis]
         u_sequence_full[~self.f_covered,:] = u_sequence
@@ -447,11 +450,11 @@ class SetCover:
         f_change = _largenumber
         while (f_change>maxfracchange) and (niters<niters_max):
             # re-initialize u
-            if (np.mod(niters, 2)==0): 
+            if (np.mod(niters, 2)==0):
                 self.reset_u(random=True)
             else:
                 self.reset_u()
-            u_tmp, Lu_tmp = self.subgradient() # find a near-optimal solution 
+            u_tmp, Lu_tmp = self.subgradient() # find a near-optimal solution
             u, Lu = self.subgradient() # rerun subgradient to get a set of Lagrangian multipliers
 
             scp_all = np.zeros(self._subg_nsteps)
@@ -465,7 +468,7 @@ class SetCover:
             imin = imin_tmp[np.argmax(Lu[imin_tmp])]
             imax = np.argmax(Lu)
             if (np.mod(niters, 5)==0):
-                print("This Best solution: UB={0}, LB={1}, UB1={2}, LB1={3}".format(scp_all[imin], Lu[imin], scp_all[imax], Lu[imax]))
+                logger.info("This Best solution: UB={0}, LB={1}, UB1={2}, LB1={3}".format(scp_all[imin], Lu[imin], scp_all[imax], Lu[imax]))
             if (niters==0) or ((scp_all[imin]<=scp_min) and ((Lu[imin]-Lu_min)>-(np.fabs(Lu_min)*self._LB_maxfracchange))):
                 scp_min = scp_all[imin]
                 u_min = np.copy(u[:,imin])
@@ -479,23 +482,23 @@ class SetCover:
             self.s = np.copy(self.f)
             UB = self.greedy()
 
-            # Which is better? absolute change or fractional change? 
+            # Which is better? absolute change or fractional change?
             # Both are fine, but cost should be normalized over the mean/median.
             GAP = (UB-LB)/np.fabs(UB)
             f_change = GAP
             if (np.mod(niters, 5)==0):
-                print("Current Best Solution: UB={0}, LB={1}, change={2}% @ niters={3}".format(UB,LB,f_change*100.,niters))
+                logger.info("Current Best Solution: UB={0}, LB={1}, change={2}% @ niters={3}".format(UB, LB, f_change*100., niters))
             niters = niters + 1
-            if (niters == niters_max): 
+            if (niters == niters_max):
                 #warnings.warn("Iteration reaches maximum = {0}".format(niters))
-                print("Iteration in re-initialization reaches maximum number = {0}".format(niters))
+                logger.info("Iteration in re-initialization reaches maximum number = {0}".format(niters))
 
         # Need to remove redundant columns
         # self.remove_redundant() # this itself is NP-hard ...
 
-        print("Current Best Solution: UB={0}, LB={1}, change={2}% @ niters={3}".format(UB,LB,f_change*100.,niters))
-        print("Final Best solution: {0}".format(UB))
+        logger.info("Current Best Solution: UB={0}, LB={1}, change={2}% @ niters={3}".format(UB, LB, f_change*100., niters))
+        logger.info("Final Best solution: {0}".format(UB))
         time_used = (time()-t0)/60.
-        print("Took {0:.3f} minutes to reach current solution.".format(time_used))
+        logger.info("Took {0:.3f} minutes to reach current solution.".format(time_used))
 
-        return (UB,time_used) 
+        return (UB,time_used)
